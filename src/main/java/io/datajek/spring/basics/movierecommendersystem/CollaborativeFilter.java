5daea4ad51745d993abef0bef7ab539c585fc968
pkpraveen603 <praveen.k@razorpay.com>
2022-04-22 03:21:39 +0530

    lesson 4: using @rimary for multiples

--- src/main/java/io/datajek/spring/basics/movierecommendersystem/CollaborativeFilter.java
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/CollaborativeFilter.java
@@ -1,5 +1,11 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class CollaborativeFilter implements Filter{
     public String[] getRecommendations(String movie){
         return new String[0];
